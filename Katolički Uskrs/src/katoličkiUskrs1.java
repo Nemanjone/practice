381a3c0a23a240a99df7dd0a3afabd00f28422cd
Marko <Marko@Nemanja>
2018-02-22 23:06:29 +0100

    Datum katoličkog Uskrsa od 1900. do 2099.

--- Katolički Uskrs/src/katoličkiUskrs1.java
+++ Katolički Uskrs/src/katoličkiUskrs1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class katolièkiUskrs {
+public class katolièkiUskrs1 {
 	
 	public static void main(String args[]) {
 		
@@ -16,17 +16,23 @@ public class katolièkiUskrs {
 		int d = (19*a + 24)%30;
 		int e = (2*b + 4*c + 6*d + 5)%7;
 		
-		int mart = 3;
-		int april = 4;
+		
 		int f = (22 + d + e);
 		int f1 = (22 + d + e - 31);
 		
+		if ( god == 1954 ||   god == 1981 || god == 2049 || god == 2076)  {
+			
 		if ( f < 31 ) {
+		    System.out.println("Uskrs godine "+ god + "." + " pada " + (f -7) + "." + "3.");
+		}else {
+			System.out.println("Uskrs godine " + god + "." + " pada " + (f1-7) + "." + "4.");
+		}
+		}
+		else if ( f < 31 ) {
 		    System.out.println("Uskrs godine "+ god + "." + " pada " + f + "." + "3.");
 		}else {
 			System.out.println("Uskrs godine " + god + "." + " pada " + f1 + "." + "4.");
 		}
-		
 		
 	}
 
